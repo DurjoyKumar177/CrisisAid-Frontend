43462760f4e042fef2e7db64537d50a8bf89598d
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-09-12 00:39:07 +0600

    remove unnecessary comments in RootLayout component

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import { Outlet } from 'react-router-dom' // Import Outlet from react-router-dom
+import { Outlet } from 'react-router-dom' 
 import Footer from '../Components/Footer'
-
-//what is outlet?
-// The Outlet component is a placeholder that renders the matched child route's element.
 
 const RootLayout = () => {
   return (
